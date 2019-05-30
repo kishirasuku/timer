d367acdcd137d99a2927925845d5ab259b99da46
18D8104032K <18D8104032K@ISE49c.educ.ise.chuo-u.ac.jp>
2019-05-30 14:24:24 +0900

    main program

---
+++ timer.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstdlib>
+#include<chrono>
+#include<thread>
+using namespace std;
+
+int main(int* argc,char* argv[]){
+  cout << "0%  10  20  30  40  50  60  70  80  90 100\n";
+  char[100] = "|---|---|---|---|---|---|---|---|---|---|\n";
+  
+}
